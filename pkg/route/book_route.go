50b0f6f3658aea596fff723463641aa7211facde
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-08-10 16:43:55 +0500

    Add routes for books

---
+++ pkg/route/book_route.go
@@ -0,0 +1,14 @@
+package route
+
+import (
+	"github.com/FarrukhMahkamov/book_management/pkg/controller"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book", controller.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controller.GetAllBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
+}
